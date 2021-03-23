8a931fe1c5448633069f64e6126ae02bcc26d09c
Dmitry Sakovich <Sakovich.dima95@gmail.ru>
2021-03-23 13:11:10 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,27 @@
+
+/* SWIPER INIT */ 
+
+var headerSwiper = new Swiper('.header-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+      },
+      /* autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      }, */
+  });
+
+  var partnerSwiper = new Swiper('.partner-swiper', {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
