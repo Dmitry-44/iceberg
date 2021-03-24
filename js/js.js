251d01a222c3f62b6e43bc53abb7149f66b35b01
Dmitry Sakovich <Sakovich.dima95@gmail.ru>
2021-03-24 08:24:19 +0300

    animation added

--- js/js.js
+++ js/js.js
@@ -25,3 +25,49 @@ var headerSwiper = new Swiper('.header-slider', {
     }
   });
 
+
+  /*  ANIMATION  */
+
+  const animItems = document.querySelectorAll('._anim-item');
+
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params) {
+      for (let index = 0; index < animItems.length; index ++){
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+
+        if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+          animItem.classList.add('_active');
+        } else {
+          if (!animItem.classList.contains('_anim-no-hide')){
+            animItem.classList.remove('_active');
+          }
+          
+        }
+
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animItemPoint;
+        }
+        console.log(animItemHeight);
+      }
+    }
+
+
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    
+    setTimeout(() => {
+      animOnScroll()
+    }, 300);
+  }
+
